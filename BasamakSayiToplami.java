b8962fcdcef60b7e061a6b54dd691bf5f03fcf41
Emre <124446702+EmreJr1@users.noreply.github.com>
2023-05-31 23:58:05 +0300

    Basamak sayılarının toplamını bulan program

---
+++ BasamakSayiToplami.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BasamakSayiToplami {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+        int toplam = 0;
+
+        while (sayi != 0) {
+            int basamak = sayi % 10;
+            toplam += basamak;
+            sayi /= 10;
+
+        }
+
+        System.out.println("Basamakların toplamı: " + toplam);
+    }
+}
